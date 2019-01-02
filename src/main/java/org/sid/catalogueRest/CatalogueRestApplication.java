29c5fd4416fa7b6b7537d0e67e921537d0cc9032
kamel-b-tahar.khenis <kamel-b-tahar.khenis@akka.eu>
2019-01-02 15:14:41 +0100

    first commit

---
+++ src/main/java/org/sid/catalogueRest/CatalogueRestApplication.java
@@ -0,0 +1,14 @@
+package org.sid.catalogueRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogueRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogueRestApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/org/sid/catalogueRest/CatalogueRestApplicationTests.java
@@ -0,0 +1,17 @@
+package org.sid.catalogueRest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CatalogueRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
