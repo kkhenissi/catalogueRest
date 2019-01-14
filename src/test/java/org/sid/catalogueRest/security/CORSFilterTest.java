a347280fbd54bf639e872ac80e0d1a15cd3fec1f
kamel-b-tahar.khenis <kamel-b-tahar.khenis@akka.eu>
2019-01-14 18:53:39 +0100

    add spring security

---
+++ src/test/java/org/sid/catalogueRest/security/CORSFilterTest.java
@@ -0,0 +1,20 @@
+package org.sid.catalogueRest.security;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CORSFilterTest {
+
+    @Test
+    public void init() {
+    }
+
+    @Test
+    public void doFilter() {
+    }
+
+    @Test
+    public void destroy() {
+    }
+}
