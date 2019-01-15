bb79f84430ed22c14cfa51e6b80d860645916bd3
kamel-b-tahar.khenis <kamel-b-tahar.khenis@akka.eu>
2019-01-15 16:21:17 +0100

    fix security on database

---
+++ src/main/java/org/sid/catalogueRest/entities/User.java
@@ -0,0 +1,4 @@
+package org.sid.catalogueRest.entities;
+
+public class User {
+}
--- src/main/java/org/sid/catalogueRest/security/SecurityConfig.java
+++ src/main/java/org/sid/catalogueRest/security/SecurityConfig.java
@@ -1,17 +1,20 @@
 package org.sid.catalogueRest.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends  WebSecurityConfigurerAdapter {
-
-
+    @Autowired
+    private UserDetailsService userDetailsService;
     // Authorization : Role -> Access
     protected  void configure(HttpSecurity http) throws  Exception
     {
@@ -22,11 +25,9 @@ public class SecurityConfig extends  WebSecurityConfigurerAdapter {
     }
 
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().passwordEncoder(
-                NoOpPasswordEncoder.getInstance()
-        ).withUser("user").password("password1")
-                .roles("USER").and().withUser("admin").password("password2")
-                .roles("USER", "ADMIN");
+        auth.userDetailsService(userDetailsService).passwordEncoder(
+                NoOpPasswordEncoder.getInstance());
+
 
     }
 
